6c4e2b6416dca58cf5c50c113b3a70e7c51c0fa9
Farzan <far.ned.me@gmail.com>
2020-07-11 21:02:07 +0430

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { SearchBoxComponent } from './movie-list/search-box/search-box.component';
+import { MovieDetailsComponent } from './movie-list/movie-details/movie-details.component';
+import { MovieItemComponent } from './movie-list/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    SearchBoxComponent,
+    MovieDetailsComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
