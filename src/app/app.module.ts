533debbd3dd19672f034e928d4075697dba76d49
Farzan <far.ned.me@gmail.com>
2020-07-11 23:55:03 +0430

    Creating templates of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { SearchBoxComponent } from './movie-list/search-box/search-box.component';
-import { MovieDetailsComponent } from './movie-list/movie-details/movie-details.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MovieItemComponent } from './movie-list/movie-item/movie-item.component';
 
 @NgModule({
